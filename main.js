9f328d96e3faed2c7604948701c4f3b6175c2cec
Hannah Hudson <hannah.hch@gmail.com>
2020-01-21 15:58:51 -0700

    added a hamburger menu and did some more styling work

---
+++ main.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+  if (!menu.classList.contains("active")) {
+    menu.classList.add("active");
+    close.classList.add("active")
+  }
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  if (close.classList.contains("active")) {
+    close.classList.remove("active");
+    menu.classList.remove("active")
+  }
+});
